130c178d11788a0b5bc900b6db4b16b5e9789dab
Meyers <augustmeyers14@gmail.com>
2025-02-25 14:26:22 -0500

    Add dir specific to locate packages

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="tornado-helper",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[
         "requests",
         "b2sdk",
