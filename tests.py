968b6f3f070f8dc0f9de1859fec1bf40602f1bcc
Meyers <augustmeyers14@gmail.com>
2025-02-25 13:42:25 -0500

    Fix test cases for main

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import logging
 import sys 
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 logging.info("Starting tests...")
 loader = unittest.TestLoader()
