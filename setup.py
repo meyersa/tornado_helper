03adb406281088b276e51ded1e15e6af635138dc
meyersa <88554476+meyersa@users.noreply.github.com>
2025-02-28 16:05:42 -0500

    Add GOES

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tornado_helper",
-    version="0.3.0",
+    version="0.5.0",
     packages=find_packages(),
     install_requires=[
         "requests",
