7772c82ea831423effec07ea56e926fc2f4961d3
meyersa <88554476+meyersa@users.noreply.github.com>
2025-03-20 17:35:19 -0400

    Increment for partial, going for bigger items

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tornado_helper",
-    version="1.0.3",
+    version="1.5.0",
     packages=find_packages(),
     install_requires=[
         "requests",
