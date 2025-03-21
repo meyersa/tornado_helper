f9ae7a8445689c616cfd8b97036b4d0ba51b135b
meyersa <88554476+meyersa@users.noreply.github.com>
2025-03-21 00:55:53 -0400

    Working examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tornado_helper",
-    version="1.6.0",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
         "requests",
