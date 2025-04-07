b26311c2950143c581670e6a37802c1d072122f3
Meyers <augustmeyers14@gmail.com>
2025-04-07 14:53:55 -0400

    Reduced to just downloading and catalog

--- tornado_helper/TorNet.py
+++ tornado_helper/TorNet.py
@@ -1,7 +1,9 @@
-from typing import List
 import logging
 from .Helper import Helper
 from pathlib import Path
+import pandas as pd 
+from typing import List, Union
+
 class TorNet(Helper):
     """
     Class for handling TorNet data downloads and uploads.
@@ -10,14 +12,8 @@ class TorNet(Helper):
     or from a specified bucket, as well as uploading data to an S3 bucket.
     """
     __DEFAULT_DATA_DIR = "./data_tornet"
-    __BUCKET_NAME = "TorNetBecauseZenodoSlow"
-    __LINKS = {
-        "bucket": [
-            "tornet_2013.tar.gz", "tornet_2014.tar.gz", "tornet_2015.tar.gz", 
-            "tornet_2016.tar.gz", "tornet_2017.tar.gz", "tornet_2018.tar.gz", 
-            "tornet_2019.tar.gz", "tornet_2020.tar.gz", "tornet_2021.tar.gz", "tornet_2022.tar.gz"
-        ],
-        "raw": [
+    __CATALOG = "https://zenodo.org/records/12636522/files/catalog.csv?download=1"
+    __LINKS =  [
             "https://zenodo.org/records/12636522/files/tornet_2013.tar.gz?download=1",
             "https://zenodo.org/records/12637032/files/tornet_2014.tar.gz?download=1",
             "https://zenodo.org/records/12655151/files/tornet_2015.tar.gz?download=1",
@@ -28,10 +24,9 @@ class TorNet(Helper):
             "https://zenodo.org/records/12655717/files/tornet_2020.tar.gz?download=1",
             "https://zenodo.org/records/12655718/files/tornet_2021.tar.gz?download=1",
             "https://zenodo.org/records/12655719/files/tornet_2022.tar.gz?download=1",
-        ],
-    }
-
-    def __init__(self, data_dir: str = None, partial: bool = True, raw: bool = False):
+        ]
+        
+    def __init__(self, data_dir: str = None):
         """
         Initializes the TorNet object with options to download raw data from Zenodo or use an existing bucket.
         
@@ -40,31 +35,30 @@ class TorNet(Helper):
             partial (bool, optional): If True, only the first dataset will be downloaded. Defaults to True.
             raw (bool, optional): If True, downloads data directly from Zenodo instead of the bucket. Defaults to False.
         """
-        self.partial = partial
-        self.raw = raw
-        
-        self.data_dir = Path(data_dir or self.__DEFAULT_DATA_DIR)
+        data_dir = Path(data_dir or self.__DEFAULT_DATA_DIR)
 
-        logging.info("TorNet initialized with partial=%s, raw=%s", partial, raw)
-        super().__init__(self.data_dir)
+        logging.info(f"TorNet initialized at {data_dir}")
+        super().__init__(data_dir)
 
-    def upload(self, files: List[str], application_key: str, application_key_id: str) -> bool:
+    def catalog(self, year: Union[int, List[int], None] = None) -> pd.DataFrame:
         """
-        Uploads the specified files to the predefined S3 bucket.
-        
-        Args:
-            files (List[str]): List of file paths to be uploaded.
-            application_key (str): Application key for authentication.
-            application_key_id (str): Application key ID for authentication.
-        
-        Returns:
-            bool: True if upload is successful, False otherwise.
+        Returns the TorNet Catalog as a DataFrame
+        If a year or list of years is provided, returns data only for those years.
+        Otherwise, returns all data.
         """
-        logging.info("Uploading files to bucket: %s", self.__BUCKET_NAME)
-        logging.debug("Files to upload: %s", files)
-        return super().upload(files, self.__BUCKET_NAME, application_key, application_key_id)
+        file_paths = super().download(self.__CATALOG)
+        df = pd.read_csv(file_paths[0])
 
-    def download(self, output_dir: str = None) -> bool:
+        if year is not None:
+            if isinstance(year, int):
+                df = df[df['Year'] == year]
+
+            elif isinstance(year, list):
+                df = df[df['Year'].isin(year)]
+
+        return df
+    
+    def download(self, partial: bool = True, output_dir: str = None) -> bool:
         """
         Downloads TorNet data based on the specified settings.
         
@@ -82,17 +76,9 @@ class TorNet(Helper):
         if not output_dir: 
             output_dir = self.data_dir
             
-        if self.raw:
-            if self.partial:
-                logging.info("Downloading single file from raw Zenodo link")
-                return super().download(self.__LINKS["raw"][0], output_dir=output_dir)
-            
-            logging.info("Downloading full dataset from raw Zenodo links")
-            return super().download(self.__LINKS["raw"], output_dir=output_dir)
-
-        if self.partial:
+        if partial:
             logging.info("Downloading single file from bucket")
-            return super().download(self.__LINKS["bucket"][0], bucket=self.__BUCKET_NAME, output_dir=output_dir)
+            return super().download(self.__LINKS[0], output_dir=output_dir)
 
         logging.info("Downloading full dataset from bucket")
-        return super().download(self.__LINKS["bucket"], bucket=self.__BUCKET_NAME, output_dir=output_dir)
+        return super().download(self.__LINKS, output_dir=output_dir)
