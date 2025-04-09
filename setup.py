6aa715f3099802c29afdd4acec08a7bf591a1dd5
meyersa <88554476+meyersa@users.noreply.github.com>
2025-04-09 08:26:09 -0400

    Add missing deps for notebook execution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tornado_helper",
-    version="2.2.0",
+    version="2.2.1",
     packages=find_packages(),
     install_requires=[
         "requests",
@@ -15,7 +15,9 @@ setup(
         "pyproj",
         "netcdf4",
         "xarray",
-        "folium"
+        "h5py",
+        "folium",
+        "ipywidgets"
     ],
     author="August Meyers",
     description="A helper package for uploading and downloading Tornado training data",
